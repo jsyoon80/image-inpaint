bafd34d037c47c2e9c5028d08e56868f5cf77061
Jongsu <80508898+jsyoon80@users.noreply.github.com>
2024-02-22 12:13:41 +0100

    Add files via upload

--- Applications/image_inpaint_streamlit.py
+++ Applications/image_inpaint_streamlit.py
@@ -28,13 +28,13 @@ st.title('이미지 인페인트 프로그램')
 st.sidebar.title('Image Inpaint')
 
 
-STREAMLIT_STATIC_PATH = pathlib.Path(st.__path__[0]) / 'static'
+#STREAMLIT_STATIC_PATH = pathlib.Path(st.__path__[0]) / 'static'
 
 # We create a downloads directory within the streamlit static asset directory
 # and we write output files to it.
-DOWNLOADS_PATH = (STREAMLIT_STATIC_PATH / "downloads")
-if not DOWNLOADS_PATH.is_dir():
-    DOWNLOADS_PATH.mkdir()
+#DOWNLOADS_PATH = (STREAMLIT_STATIC_PATH / "downloads")
+#if not DOWNLOADS_PATH.is_dir():
+#    DOWNLOADS_PATH.mkdir()
 
 
 def get_image_download_link(img, filename, text):
